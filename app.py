95a559a132681373ed28306c61693aab25805aac
tarannumaara2000-a11y <tarannum.aara2000@gmail.com>
2026-03-12 13:30:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GitHub Actions Deployment test"
+    return "Hello from GitHub Actions Deployment tested"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
